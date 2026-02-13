e86539149a6525a37dfa798ec509a354572173a1
Misas <paula.carolina.misas@accenture.com>
2026-02-13 18:03:35 -0500

    Agregar repositorio en memoria para warehouses e interfaz

---
+++ services/warehouse-service/src/main/java/com/fulfillment/warehouseservice/domain/port/WarehouseRepository.java
@@ -0,0 +1,14 @@
+package com.fulfillment.warehouseservice.domain.port;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.fulfillment.warehouseservice.domain.model.Warehouse;
+
+public interface WarehouseRepository {
+
+    Warehouse save(Warehouse warehouse);
+    Optional<Warehouse> findById(String warehouseId);
+    List<Warehouse> findall();
+    
+}
