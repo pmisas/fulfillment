1e602e0e926d115957e16ca155e0d43c9548cf6c
Misas <paula.carolina.misas@accenture.com>
2026-02-13 18:03:35 -0500

    Agregar repositorio en memoria para warehouses e interfaz

---
+++ services/warehouse-service/src/main/java/com/fulfillment/warehouseservice/infrastructure/repository/memory/InMemoryRespositoryAdapter.java
@@ -0,0 +1,31 @@
+package com.fulfillment.warehouseservice.infrastructure.repository.memory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.fulfillment.warehouseservice.domain.model.Warehouse;
+import com.fulfillment.warehouseservice.domain.port.WarehouseRepository;
+
+public class InMemoryRespositoryAdapter implements WarehouseRepository {
+    
+    private final ConcurrentMap<String, Warehouse> db = new ConcurrentHashMap<>();
+    
+    @Override
+    public Warehouse save(Warehouse warehouse) {
+        db.put(warehouse.getWarehouseId(), warehouse);
+        return warehouse;
+    }
+
+    @Override
+    public Optional<Warehouse> findById(String warehouseId) {
+        return Optional.ofNullable(db.get(warehouseId));
+    }
+
+    @Override
+    public List<Warehouse> findall(){
+        return new ArrayList<>(db.values());
+    }
+}
