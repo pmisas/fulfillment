4f634731a27b6ab598aa1a55302e500e0ac817b3
Misas <paula.carolina.misas@accenture.com>
2026-02-15 02:33:24 -0500

    Agregar annotation de repository

--- services/warehouse-service/src/main/java/com/fulfillment/warehouseservice/infrastructure/repository/memory/InMemoryRespositoryAdapter.java
+++ services/warehouse-service/src/main/java/com/fulfillment/warehouseservice/infrastructure/repository/memory/InMemoryRespositoryAdapter.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.stereotype.Repository;
+
 import com.fulfillment.warehouseservice.domain.model.Warehouse;
 import com.fulfillment.warehouseservice.domain.port.WarehouseRepository;
 
+@Repository
 public class InMemoryRespositoryAdapter implements WarehouseRepository {
     
     private final ConcurrentMap<String, Warehouse> db = new ConcurrentHashMap<>();
