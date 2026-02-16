918feba21eca07fd8718e0c910046d7359f3f3cb
Misas <paula.carolina.misas@accenture.com>
2026-02-15 20:58:54 -0500

    Agregar consulta de order-service a order-warehouse

---
+++ services/order-service/src/main/java/com/fulfillment/orderservice/infrastructure/rest/client/WarehouseHttpClient.java
@@ -0,0 +1,32 @@
+package com.fulfillment.orderservice.infrastructure.rest.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import com.fulfillment.orderservice.domain.port.WarehouseClient;
+
+@Component
+public class WarehouseHttpClient implements WarehouseClient{
+    
+    private final RestClient client;
+    
+    public WarehouseHttpClient(@Value("${warehouse.service.base-url}")String baseUrl) {
+        this.client = RestClient.builder()
+                    .baseUrl(baseUrl)
+                    .build();
+    }
+
+    @Override
+    public boolean anyWarehouseExists() {
+        ResponseEntity<Void> resp = client.get()
+                .uri("/api/v1/warehouses/exists")
+                .retrieve()
+                .toBodilessEntity();
+
+        return resp.getStatusCode().value() == 200;
+
+    }
+
+}
