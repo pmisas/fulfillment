478d1038c8516e997a9a5e6110e93d03c53d24c7
Misas <paula.carolina.misas@accenture.com>
2026-02-16 02:42:37 -0500

    Agregar GlobalExeptionHandler para order y warehouse

---
+++ services/order-service/src/main/java/com/fulfillment/orderservice/infrastructure/rest/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.fulfillment.orderservice.infrastructure.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.fulfillment.orderservice.domain.exception.IdempotencyInconsistentStateException;
+import com.fulfillment.orderservice.domain.exception.OrderNotFoundException;
+import com.fulfillment.orderservice.domain.exception.WarehouseNotAvailableException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(WarehouseNotAvailableException.class)
+    @ResponseStatus(HttpStatus.CONFLICT) // devuelve 409
+    public String handleWarehouseNotAvailable(WarehouseNotAvailableException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // devuelve 404
+    public String handleOrderNotFound(OrderNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(IdempotencyInconsistentStateException.class)
+    @ResponseStatus(HttpStatus.CONFLICT) // devuelve 409
+    public String handleIdempotency(IdempotencyInconsistentStateException ex) {
+        return ex.getMessage();
+    }
+}
