e289307fcef5829254be905f06e100824f216c8a
Misas <paula.carolina.misas@accenture.com>
2026-04-09 02:15:49 -0500

    Agregar tablas warehouse_access y otbox_events a terraform

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -53,4 +53,76 @@ resource "aws_dynamodb_table" "warehouses" {
   }
 }
 
+resource "aws_dynamodb_table" "inventory_items" {
+  name         = "InventoryItems"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "warehouseId"
+  range_key    = "sku"
+
+  attribute {
+    name = "warehouseId"
+    type = "S"
+  }
+
+  attribute {
+    name = "sku"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "outbox_events" {
+  name         = "OutboxEvents"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "eventId"
+
+  attribute {
+    name = "eventId"
+    type = "S"
+  }
+
+  attribute {
+    name = "publishStatus"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name            = "ByPublishStatus"
+    hash_key        = "publishStatus"
+    range_key       = "createdAt"
+    projection_type = "ALL"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+}
+
+resource "aws_dynamodb_table" "warehouse_access" {
+  name         = "WarehouseAccess"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "warehouseId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "warehouseId-userId-index"
+    hash_key        = "warehouseId"
+    range_key       = "userId"
+    projection_type = "ALL"
+  }
+}
 
